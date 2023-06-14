fc0fb35b2c7f84c191db9f9efec5bbe599369f9d
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-06-15 01:34:06 +0600

    Categories Started

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../Shared/Container';
+
+const Categories = () => {
+    return (
+        <Container>
+            Hello From Categories
+        </Container>
+    );
+};
+
+export default Categories;
